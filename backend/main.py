93b30019294d684e5d525cff8ec46ede735e5482
Fablius <fablius2005@gmail.com>
2026-06-03 22:56:34 +0800

    shorten entity cluster names to 3 words

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ class SearchRequest(BaseModel):
 
 def extract_entities(text):
     doc = nlp(text[:10000])
-    entities = [(ent.text, ent.label_) for ent in doc.ents]
+    entities = [(" ".join(ent.text.split()[:3]), ent.label_) for ent in doc.ents]
     return entities
 
 def scrape_article(url):
